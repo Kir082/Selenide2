6e88af636573845b2bf22a0440242ba4407f5675
kir082 <kirillich082@gmail.com>
2025-05-20 09:40:50 +0300

    rename BeforeAll, delete sleep()

--- src/test/java/SelenideTestDragAndDrop.java
+++ src/test/java/SelenideTestDragAndDrop.java
@@ -1,18 +1,14 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.DragAndDropOptions;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.DragAndDropOptions.to;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTestDragAndDrop {
     @BeforeAll
-    static void beforeAll() {
+    static void setupConfig() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
     }
@@ -23,10 +19,8 @@ public class SelenideTestDragAndDrop {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").shouldHave(text("A"));
         $("#column-b").shouldHave(text("B"));
-        sleep(2000);
         $("#column-a").dragAndDrop(to($("#column-b")));
         $("#column-a").shouldHave(text("B"));
-        sleep(2000);
     }
 
     @Test
@@ -35,10 +29,8 @@ public class SelenideTestDragAndDrop {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").shouldHave(text("A"));
         $("#column-b").shouldHave(text("B"));
-        sleep(1000);
         actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
         $("#column-a").shouldHave(text("B"));
-        sleep(1000);
 
     }
 }
